b9f0b9202e4b0d4f6f22d967b2dd3cec5d1f9b80
pokebox <pokebox@qq.com>
2025-12-25 01:14:07 +0800

    排序数据，调整注释

--- Const.h
+++ Const.h
@@ -2,20 +2,58 @@
 #include <cstdint>
 
 // CAN FRAME IDs
-#define STEERING_SENSORS_ID 342
+// 快速周期0.04s左右频率更新，约为20Hz
+// 344 380 | 304 330 148 274 320
 #define ENGINE_DATA_ID 344 // 注意：从 ENGINE_DATA_2_ID 改为 ENGINE_DATA_ID
-#define WHEEL_SPEEDS_ID 464
-#define VSA_STATUS_ID 420
 #define POWERTRAIN_DATA_ID 380
-#define VEHICLE_DYNAMICS_ID 490
-#define GEARBOX_ID 401 // 注意：之前是 422，根据DBC应该是401
-#define STANDSTILL_ID 432
-#define DRIVE_MODES_ID 545
-#define ENGINE_DATA_3_ID 804
-#define CAR_SPEED_ID 777
-#define DOORS_STATUS_ID 1029 // 添加车门状态ID
+//增加的
+#define GAS_PEDAL_2_ID 304
+#define STEERING_SENSORS_ID 330
+#define KINEMATICS_ALT_ID 148
 
-// 新增变速箱ID（根据DBC）
+// 0.08s左右频率更新，约为10Hz
+// 420 432 464 490 | 427 493 450 | 470 476 507 428
+#define VSA_STATUS_ID 420
+
+#define STANDSTILL_ID 432
+#define WHEEL_SPEEDS_ID 464
+#define VEHICLE_DYNAMICS_ID 490
+
+#define STEER_MOTOR_TORQUE_ID 427
+#define HUD_SETTING_ID 493
+#define EPB_STATUS_ID 450
+
+// 0.16s左右频率更新，约为5Hz
+// 545 | 597 662 | 57
+#define DRIVE_MODES_ID 545
+
+#define ROUGH_WHEEL_SPEED_ID 597
+#define SCM_BUTTONS_ID 662
+
+// 0.4s左右频率更新，约为2.5Hz
+// 777 804 795 806 881 882 884 888 891 918 800 808 815 825 846 773
+#define CAR_SPEED_ID 777
+#define CRUISE_ID 804
+
+#define SCM_FEEDBACK_ID 806
+#define STALK_STATUS_ID 884
+#define STALK_STATUS_2_ID 891
+#define SEATBELT_STATUS_ID 773
+
+// 0.8s左右频率更新，约为1.25Hz
+// 929 983 985
+// 1.2s左右频率更新，约为0.8Hz
+// 1029 1115 1064 1024 1036 1108 1125 1127
+#define DOORS_STATUS_ID 1029 // 添加车门状态ID
+// 2s左右频率更新，约为0.5Hz
+// 1302 1296 1365 1361 1633
+#define ODOMETER_ID 1302
+
+// 4s左右频率更新，约为0.25Hz
+// 1424
+
+// 变速箱ID
+#define GEARBOX_ID 401     // 注意：之前是 422，根据DBC应该是401
 #define GEARBOX_422_ID 422 // 如果同时存在两个变速箱ID
 
 typedef enum
@@ -63,59 +101,58 @@ typedef enum
     EPB_STATE_ON = 3
 } EPB_STATE_t;
 
-// ID: 380 - POWERTRAIN_DATA
+// ID: 380 - POWERTRAIN_DATA 动力总成数据
 typedef struct
 {
-    uint8_t PEDAL_GAS;     // 7|8@0+ (1,0) [0|255] EON
-    uint16_t ENGINE_RPM;   // 23|16@0+ (1,0) [0|15000] "rpm" EON
-    uint8_t GAS_PRESSED;   // 39|1@0+ (1,0) [0|1] EON
-    uint8_t ACC_STATUS;    // 38|1@0+ (1,0) [0|1] EON (新增)
-    uint8_t BOH_17C;       // 37|5@0+ (1,0) [0|1] EON (新增)
-    uint8_t BRAKE_SWITCH;  // 32|1@0+ (1,0) [0|1] EON
-    uint8_t BOH2_17C;      // 47|10@0+ (1,0) [0|1] EON (新增)
-    uint8_t BRAKE_PRESSED; // 53|1@0+ (1,0) [0|1] EON
-    uint8_t BOH3_17C;      // 52|5@0+ (1,0) [0|1] EON (新增)
-    uint8_t COUNTER;       // 61|2@0+ (1,0) [0|3] EON (新增)
-    uint8_t CHECKSUM;      // 59|4@0+ (1,0) [0|15] EON (新增)
+    uint8_t PEDAL_GAS;     // 油门踏板 [0|255]
+    uint16_t ENGINE_RPM;   // 发动机转速 [0|15000] rpm
+    uint8_t GAS_PRESSED;   // 
+    uint8_t ACC_STATUS;    //
+    uint8_t BOH_17C;       //
+    uint8_t BRAKE_SWITCH;  //
+    uint8_t BOH2_17C;      //
+    uint8_t BRAKE_PRESSED; //
+    uint8_t BOH3_17C;      //
+    uint8_t COUNTER;       //
+    uint8_t CHECKSUM;      //
 } POWERTRAIN_DATA;
 
-// ID: 344 - ENGINE_DATA (原ENGINE_DATA_2)
+// ID: 344 - ENGINE_DATA 发动机数据
 typedef struct
 {
-    float XMISSION_SPEED;  // 7|16@0+ (0.01,0) [0|250] "kph" EON
-    uint16_t ENGINE_RPM;   // 23|16@0+ (1,0) [0|15000] "rpm" EON (新增)
-    float XMISSION_SPEED2; // 39|16@0+ (0.01,0) [0|250] "kph" EON (新增)
-    uint16_t ODOMETER;     // 55|8@0+ (10,0) [0|2550] "m" (新增)
-    uint8_t COUNTER;       // 61|2@0+ (1,0) [0|3] EON (新增)
-    uint8_t CHECKSUM;      // 59|4@0+ (1,0) [0|15] EON (新增)
+    float XMISSION_SPEED;  //
+    uint16_t ENGINE_RPM;   //
+    float XMISSION_SPEED2; //
+    uint16_t ODOMETER;     //
+    uint8_t COUNTER;       //
+    uint8_t CHECKSUM;      //
 } ENGINE_DATA;
 
-// ID: 420 - VSA_STATUS
+// ID: 420 - VSA_STATUS 车辆状态数据
 typedef struct
 {
-    uint16_t USER_BRAKE;        // 7|16@0+ (0.015625,-1.609375) [0|1000] EON
-    uint8_t COMPUTER_BRAKING;   // 23|1@0+ (1,0) [0|1] EON (新增)
-    uint8_t ESP_DISABLED;       // 28|1@0+ (1,0) [0|1] EON
-    uint8_t BRAKE_HOLD_RELATED; // 52|1@0+ (1,0) [0|1] XXX (新增)
-    uint8_t BRAKE_HOLD_ACTIVE;  // 46|1@0+ (1,0) [0|1] EON (新增)
-    uint8_t BRAKE_HOLD_ENABLED; // 45|1@0+ (1,0) [0|1] EON (新增)
-    uint8_t COUNTER;            // 61|2@0+ (1,0) [0|3] EON (新增)
-    uint8_t CHECKSUM;           // 59|4@0+ (1,0) [0|15] EON (新增)
+    uint16_t USER_BRAKE;        //
+    uint8_t COMPUTER_BRAKING;   //
+    uint8_t ESP_DISABLED;       //
+    uint8_t BRAKE_HOLD_RELATED; //
+    uint8_t BRAKE_HOLD_ACTIVE;  //
+    uint8_t BRAKE_HOLD_ENABLED; //
+    uint8_t COUNTER;            //
+    uint8_t CHECKSUM;           //
 } VSA_STATUS;
 
-// ID: 1029 - DOORS_STATUS
+// ID: 1029 - DOORS_STATUS 车门状态数据
 typedef struct
 {
-    uint8_t DOOR_OPEN_FL; // 37|1@0+ (1,0) [0|1] EON (新增)
-    uint8_t DOOR_OPEN_FR; // 38|1@0+ (1,0) [0|1] EON (新增)
-    uint8_t DOOR_OPEN_RL; // 39|1@0+ (1,0) [0|1] EON (新增)
-    uint8_t DOOR_OPEN_RR; // 40|1@0+ (1,0) [0|1] EON (新增)
-    uint8_t TRUNK_OPEN;   // 41|1@0+ (1,0) [0|1] EON (新增)
-    uint8_t COUNTER;      // 61|2@0+ (1,0) [0|3] EON (新增)
-    uint8_t CHECKSUM;     // 59|4@0+ (1,0) [0|15] EON (新增)
+    uint8_t DOOR_OPEN_FL; //
+    uint8_t DOOR_OPEN_FR; //
+    uint8_t DOOR_OPEN_RL; //
+    uint8_t DOOR_OPEN_RR; //
+    uint8_t TRUNK_OPEN;   //
+    uint8_t COUNTER;      //
+    uint8_t CHECKSUM;     //
 } DOORS_STATUS;
 
-// 其他现有结构体保持不变...
 typedef struct
 {
     uint8_t GEAR_SHIFTER; // 5|6@0+ (1,0) [0|63] "" EON
@@ -141,17 +178,17 @@ typedef struct
 
 typedef struct
 {
-    float WHEEL_SPEED_FL;
-    float WHEEL_SPEED_FR;
-    float WHEEL_SPEED_RL;
-    float WHEEL_SPEED_RR;
-} WHEEL_SPEEDS;
+    float WHEEL_SPEED_FL;    //kph
+    float WHEEL_SPEED_FR;   //kph
+    float WHEEL_SPEED_RL;   //kph
+    float WHEEL_SPEED_RR;   //kph
+} WHEEL_SPEEDS;     //车轮速度数据
 
 typedef struct
 {
-    float LAT_ACCEL;
-    float LONG_ACCEL;
-} VEHICLE_DYNAMICS;
+    float LAT_ACCEL;    //m/s^2
+    float LONG_ACCEL;   //m/s^2
+} VEHICLE_DYNAMICS;     //车辆动力学数据
 
 typedef struct
 {
